9d4442811e7892b3112d491e3763638c4429818d
Dishank7 <127612933+Dishank7@users.noreply.github.com>
2023-06-03 22:28:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function volume_sphere() {
     //Write your code here
+	let radius = document.getElementById("radius");
+    let radiusValue = radius.value ;
+    let volume = document.getElementById("volume");
+    let ans = 4/3*3.14*radiusValue*radiusValue*radiusValue;
+    volume.value =ans;
   
 } 
 
